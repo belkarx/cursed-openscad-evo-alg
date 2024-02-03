c9edf4b3013012284015f008958d80f908951ff0
belkarx <you@example.com>
2024-02-03 13:13:26 -0800

    new structure done

--- openscad_gen.py
+++ openscad_gen.py
@@ -17,6 +17,17 @@ class FibonacciIter:
         self.a, self.b = self.b, self.a + self.b
         return result
 
+class UniformRandomIter:
+    def __init__(self, bottom_range, top_range):
+        self.bottom = bottom_range
+        self.top = top_range
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return random.randint(self.bottom, self.top)
+
 class StableIter:
     def __init__(self, limit):
         self.limit = limit
@@ -25,9 +36,8 @@ class StableIter:
         return self
 
     def __next__(self):
-        return self
+        return self.limit
 
-fib = FibonacciIter(10)
 nrm = np.random.default_rng() #rng.normal(0, 1)
 
 #itr1=number generator, governs complexity | stb (stable) uniform (incl 0), fibonacci (up to limit), random (normal(x, y)), random (uniform from x to y), exponential (up to limit), linear(1,5)
@@ -42,10 +52,10 @@ nrm = np.random.default_rng() #rng.normal(0, 1)
 # itrtyp-itr implies itr-num# (as a limit)
 
 def scale(x, y, z):
-    return f"scale([{x}, {y}, {z}])"
+    return f"scale([{x.next()}, {y.next()}, {z.next()}])"
 
 def translate(x, y, z):
-    return f"translate([{x}, {y}, {z}])"
+    return f"translate([{x.next()}, {y}, {z}])"
 
 def rotate(x, y, z):
     return f"rotate([{x}, {y}, {z}])"
@@ -53,7 +63,7 @@ def rotate(x, y, z):
 print("$fn = 100;")
 
 # 10 shapes, 10 cubes then 10 spheres, cubes transformed up according to fibonacci sequence (scaled by .5) and rotated uniformly by 1, scaled by linear random generator, spheres translated to left by 1 each
-seed_cube = "10-mov-fib-MAX-stb-0-stb-0-rot-stb-0.5-stb-0.5-stb-0.5-scl-lin-0,5-lin-0,5-lin-0,5"
+seed_cube = "10-mov-fib-MAX-stb-0-stb-0-rot-stb-0.5-stb-0.5-stb-0.5"#-scl-lin-0,5-lin-0,5-lin-0,5"
 
 seed_cube = seed_cube.split("-")
 
@@ -65,6 +75,13 @@ def get_number_gen(number_gen, seed_cube):
         return FibonacciIter(seed_cube.next())
     elif number_gen == "stb":
         return StableIter(seed_cube.next())
+
+trfm_generators = [shapes_to_act_on_number_and_type: [functions:[parameters]]]
+
+def sphere():
+    return "sphere(1);"
+
+structure = [{"shape":sphere(), number: 10, transforms:[translate(StableIter(0), StableIter(0), FibonacciIter(number)), rotate(Stable(0.5), StableIter(0.5), StableIter(0.5), scale(UniformRandomIter(0, 5), UniformRandomIter(0,5), UniformRandomIter(0,5)]}]
 
 for cdn in seed_cube:
     if cdn in ["mov", "rot", "scl"]:
@@ -85,17 +102,3 @@ for cdn in seed_cube:
             number_gen_3 = get_number_gen(seed_cube.next(), seed_cube)
         
             print(transform(number_gen_1.next(), number_gen_2.next(), number_gen_3.next()))
-        
-        translate(
-    for i in range(n_shapes):
-        print(translate(number_gen.next(), number_gen.next(), number_gen.next()))
-        print("cube(1);")
-
-    
-
-for i in range(n):
-    case = random.randint(0, 5)
-    if case > 1:
-        print(scale(rng.normal(0, 2), rng.normal(0, 2), rng.normal(0, 2)))
-    print(translate(rng.normal(0, d), rng.normal(0, d), rng.normal(0, d)))
-    print("cube(1);")
