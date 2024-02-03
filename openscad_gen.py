aba6d77bce14a390b946ba1cfb9b5a0af65262e8
belkarx <you@example.com>
2024-02-03 15:51:00 -0800

    failed at separation of params and iters

--- openscad_gen.py
+++ openscad_gen.py
@@ -5,25 +5,15 @@ from pprint import pprint
 import os
 import pyperclip
 
-nrm = np.random.default_rng() #rng.normal(0, 1)
+rng = np.random.default_rng() #rng.normal(0, 1)
 
-#itr1=number generator, governs complexity | stb (stable) uniform (incl 0), fibonacci (up to limit), random (normal(x, y)), random (uniform from x to y), exponential (up to limit), linear(1,5)
-#itr2
-#num=base number | 0-10 to start
-#shp={shapes} | cube(1), sphere(1) to start
-#tfm={transformations} | translate, scale, rotate to start
-
-#itr-num#-itr-itr-shp-itr-itr-tfm
-
-# trm implies (itr-itr-itr)
-# itrtyp-itr implies itr-num# (as a limit)
-
-#seed_cube = "10-mov-fib-MAX-stb-0-stb-0-rot-stb-0.5-stb-0.5-stb-0.5"#-scl-lin-0,5-lin-0,5-lin-0,5"
+#random (normal(x, y))  
+#exponential, log
 
 # iterators
 class FibIter:
-    def __init__(self, limit):
-        self.limit = limit
+    def __init__(self, limit=None):
+        #limit exists just for Compatibility
         self.a, self.b = 0, 1
 
     def __iter__(self):
@@ -34,10 +24,13 @@ class FibIter:
         self.a, self.b = self.b, self.a + self.b
         return result
 
+    def reset(self):
+        self.a, self.b = 0, 1
+
 class UniformRandomIter:
-    def __init__(self, bottom_range, top_range):
-        self.bottom = bottom_range
-        self.top = top_range
+    def __init__(self, rnge):
+        self.bottom = rnge[0]
+        self.top = rnge[1]
 
     def __iter__(self):
         return self
@@ -72,61 +65,82 @@ def cube():
     return "cube(1);"
 
 
-structure = [
+seeds = [[
     {
         "shape": sphere,
-        "number": number,
-        "fn": 100,
+        "number": 10,
+        "fn": 50,
         "transforms": [
             {
                 "function": translate,
-                "parameters": [StableIter(0), StableIter(0), FibIter(number)],
+                "generators": [StableIter, StableIter, FibIter],
+                "parameters": [0, 0, None],
             },
             {
                 "function": rotate,
-                "parameters": [StableIter(45), StableIter(45), FibIter(number)],
+                "generators": [StableIter, StableIter, FibIter],
+                "parameters": [45, 45, None],
             },
             {
                 "function": scale,
-                "parameters": [UniformRandomIter(1, 5), UniformRandomIter(1, 5), UniformRandomIter(1, 5)],
+                "generators": [UniformRandomIter, UniformRandomIter, UniformRandomIter],
+                "parameters": [[1, 5], [1, 5], [1, 5]],
             }
         ],
     }
-]
+]]
+
+f = FibIter()
 
 def render(structure):
     to_paste = f"$fn = {structure[0]['fn']};\n"
+
     for shape in structure:
         for i in range(shape["number"]):
             for transform in shape["transforms"]:
-                to_paste += transform["function"](*transform["parameters"]) + "\n"
-                print(transform["function"](*transform["parameters"]))
-            to_paste += shape["shape"]() + "\n"
-            print(shape["shape"]())
+                to_paste += transform["function"](*[generator(parameter) for generator, parameter in zip(transform["generators"], transform["parameters"])]) + "\n"
 
+                for g in transform["generators"]:
+                    if g == FibIter:
+                        g = f
+            to_paste += shape["shape"]() + "\n"
+
+
+    print(to_paste)
     pyperclip.copy(to_paste)
     os.system("sh shopenscad.sh")
+
+# change fn
+# add, remove a transform
+# change the iterator of a transform
+# change parameters of iterator
+# change number
+# change shapev
+
+def mutate(structure, good, bad):
+    print("mutating")
+    for s in structure:
+        s['fn'] = abs(int(rng.normal(s['fn'], 50)))
+    return structure
+
 
 good = []
 bad = []
 
-# rlhf mutate loop
-
-for i in range(5):
-    render(structure)
+for s in seeds:
+    render(s)
     if input() != "":
-        good.append(structure)
-    else:
-        bad.append(structure)
+        good.append(s.copy())
+    print("DONE RENDERING")
+    #else:
+    #    bad.append(structure.copy())
 
-print(good)
-print(bad)
-
-def mutate(structure):
-    # change fn
-    # add, remove a transform
-    # change the iterator of a transform
-    # change parameters of iterator
-    # change number
-    # change shape
-    pass
+for g in good:
+    print(g)
+    print()
+    new_seed = mutate(g.copy(), good, bad)
+    print(new_seed)
+    render(new_seed)
+    
+    print()
+    print("----")
--- shopenscad.sh
+++ shopenscad.sh
@@ -3,3 +3,4 @@ xdotool key Control_L+a
 xdotool key BackSpace
 xdotool key Control_L+v
 xdotool mousemove --sync 1230 80 click 1
+xdotool key Super_L+Right
