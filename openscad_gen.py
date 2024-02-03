bb64c1bd74bfdefdce5b71720a9431ed86e8af4c
belkarx <you@example.com>
2024-02-03 12:22:33 -0800

    reworking structure

--- openscad_gen.py
+++ openscad_gen.py
@@ -2,13 +2,32 @@ import random
 import numpy as np
 import sys
 
-rng = np.random.default_rng()
+class FibonacciIter:
+    def __init__(self, limit):
+        self.limit = limit
+        self.a, self.b = 0, 1
 
-d = float(sys.argv[1])
-try:
-    n = int(sys.argv[2])
-except:
-    n = 10
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.a > self.limit:
+            raise StopIteration
+        result = self.a
+        self.a, self.b = self.b, self.a + self.b
+        return result
+
+fib = FibonacciIter(10)
+nrm = np.random.default_rng() #rng.normal(0, 1)
+
+
+#itr=number generator, governs complexity | uniform, 0, fibonacci, random (normal(x, y)), exponential
+#num=base number | 0-10 to start
+#shp={shapes} | cube, sphere to start
+#tfm={transformations} | translate, scale, rotate to start
+
+#itr-num-itr-itr-shp-itr-itr-tfm
+
 
 def scale(x, y, z):
     return f"scale([{x}, {y}, {z}])"
